4be545b3dc311229773a3f9882ad8417dc42bcaa
OyoCute <98724698+OyoCute@users.noreply.github.com>
2022-02-17 17:31:13 +0800

    Add files via upload

---
+++ exer.js
@@ -0,0 +1,11 @@
+function run() {
+    let a = document.getElementById("input_word").value;
+
+    let sorted = (a) => (a)
+    .split("")
+    .sort()
+    .join("");
+    console.log(sorted(a))
+
+    document.getElementById("output").innerHTML = sorted(a);
+}
